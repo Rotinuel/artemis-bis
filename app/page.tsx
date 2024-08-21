9a7cd2dc35f01a423eb4205697ef221a6d815901
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2024-08-21 19:13:46 +0100

    updated the favicon

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-400 to-blue-800">
+      <div className="space-y-6">
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">Artemis-Bis</h1>
+      </div>
 
     </main>
   );
