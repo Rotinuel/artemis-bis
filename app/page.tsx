a643731ddb8da044ff27651810a623853f054694
Immanuel Okhuarobo <emmanuelisagenius@gmail.com>
2024-08-21 19:16:17 +0100

    adding an emoji to it

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-purple-400 to-blue-800">
       <div className="space-y-6">
-        <h1 className="text-6xl font-semibold text-white drop-shadow-md">Artemis-Bis</h1>
+        <h1 className="text-6xl font-semibold text-white drop-shadow-md">🧭Artemis-Bis</h1>
       </div>
 
     </main>
